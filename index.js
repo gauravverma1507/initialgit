ab978a1155b5ee94ff6076b7c650daa916509b21
Gaurav verma <gauravverma1507@yahoo.com>
2022-09-25 14:31:30 +0530

    newchange

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log('hello');
 console.log(5);
 
 console.log("hello deaR");
+
+
+console.log('first we will make our repository on github');
+console.log('then we will use this cmd in git bash');
+console.log('git remote add origin https://github.com/gauravverma1507/initialgit.git');
+console.log('THen for push we will our code into that server on the github');
+console.log('git push -u origin master');
